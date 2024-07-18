82b11e6918ccbc56037b734f7f6e5b07daed6aab
mhmodfrmwi <mahmoudframawi98@gmail.com>
2024-07-18 14:09:29 +0300

    resolving issues in state and adding some changes for good user experience

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,57 +1,63 @@
+import React, { useEffect, useState } from "react";
+import { CircularProgress } from "@chakra-ui/react";
 import Article from "@/components-2/Article";
 import Header from "@/components-2/Header";
-import { useEffect, useState } from "react";
 
 const Home = () => {
   const [articles, setArticles] = useState([]);
-  const [error, setError] = useState(null);
+  const [error, setError] = useState(false);
   const [category, setCategory] = useState("general");
   const [country, setCountry] = useState("au");
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    console.log(`Category: ${category}, Country: ${country}`);
     const url = `https://saurav.tech/NewsAPI/top-headlines/category/${category}/${country}.json`;
+    setLoading(true);
+    setError(false);
     fetch(url)
-      .then((res) => {
-        if (!res.ok) {
-          throw new Error("Network response was not ok: " + res.statusText);
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
         }
-        return res.json();
+        return response.json();
       })
       .then((data) => {
         if (data.articles) {
           setArticles(data.articles);
-          localStorage.setItem("news", JSON.stringify(data.articles));
         } else {
           throw new Error("Data format is not as expected");
         }
+        setTimeout(() => {
+          setLoading(false); // Ensure loading state is set to false here
+        }, 500);
       })
-      .catch((error) => {
-        setError(error.message);
-        console.error("Fetching error:", error);
+      .catch(() => {
+        setError(true);
+        setTimeout(() => {
+          setLoading(false); // Ensure loading state is set to false here
+        }, 2000); // Ensure loading state is set to false here as well
       });
   }, [category, country]);
 
   return (
     <div className="flex flex-col gap-5">
       <Header onCategoryChange={setCategory} onCountryChange={setCountry} />
-      <div className="flex flex-col gap-2 p-4 w-full">
-        {error ? (
-          <p>Error: {error}</p>
-        ) : articles.length > 0 ? (
-          articles.map((article, index) => (
-            <Article
-              key={index}
-              image={article.urlToImage}
-              title={article.title}
-              description={article.description}
-              author={article.author}
-              publishedAt={article.publishedAt}
-            />
-          ))
-        ) : (
-          <p>No articles found</p>
-        )}
+      <div className="flex flex-col gap-2 p-4 w-full items-center">
+        {loading && <CircularProgress isIndeterminate />}
+        {error && <p>Something went wrong..!</p>}
+        {!loading && !error && articles.length > 0
+          ? articles.map((article, index) => (
+              <Article
+                key={index}
+                image={article.urlToImage}
+                title={article.title}
+                description={article.description}
+                content={article.content}
+                author={article.author}
+                publishedAt={article.publishedAt}
+              />
+            ))
+          : !loading && !error && <p>No articles found</p>}
       </div>
     </div>
   );
