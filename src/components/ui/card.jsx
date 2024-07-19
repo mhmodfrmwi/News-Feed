887eb8673a944d7238e0d4fe1ac187e41ac4870d
mhmodfrmwi <mahmoudframawi98@gmail.com>
2024-07-19 22:52:17 +0300

    color fix

--- src/components/ui/card.jsx
+++ src/components/ui/card.jsx
@@ -6,7 +6,7 @@ const Card = React.forwardRef(({ className, ...props }, ref) => (
   <div
     ref={ref}
     className={cn(
-      "rounded-lg border-0 bg-slate-200 text-card-foreground shadow-sm dark:bg-sky-950",
+      "rounded-lg border-0 bg-slate-300 text-card-foreground shadow-sm dark:bg-sky-950",
       className
     )}
     {...props}
